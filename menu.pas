dd0f0d9d7386472e81f362f7597045d47266e3d7
rob0229 <rob0229@gmail.com>
2015-04-02 12:43:59 -0400

    added menu

--- menu.pas
+++ menu.pas
@@ -5,6 +5,12 @@ procedure displayMenu();
 implementation 
 procedure displayMenu();
 	begin
-		writeln('This is the menu');
+		writeln('Choose an option');
+		writeln('1- Create a function');
+		writeln('2- Call Function');
+		writeln('3- Simulate Function Return');
+		writeln('4- Print Current Stack');
+		writeln('5- Reset Simulation');
+		writeln('6- Exit');
 	end;
 end.
