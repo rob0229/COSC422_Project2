e98521a4c0a7f7c9838bff770083c3a61d94b803
rob0229 <rob0229@gmail.com>
2015-04-04 20:58:08 -0400

    line 136 ptr error at runtime. Solution may be to use array index instead of pointers

--- functions.pas
+++ functions.pas
@@ -117,46 +117,34 @@ function callFunction(func_array: array of function_info; stack: activation_reco
                 new_record.locals[i].var_value := param_value;    
             end;
 
-      
-		
-
 		{* Add the new activation record to the stack *}
 		setlength(new_stack, stack_size);
 		 writeln('made it to here');
 		new_stack[stack_size - 1] := new_record;
- writeln('here 1');
 
 		{*set control_link, access_link, and return pointers*}
 		if (stack_size > 2) then
 			begin
 		    	new_stack[stack_size - 1].control_link^ := new_stack[stack_size - 2];
-
-writeln('here 2');
-
 			end
 		else
 			begin
 
 writeln('here 3: ', new_stack[stack_size - 1].name);
+writeln(new_stack[0].name);
 
 				 new_stack[stack_size - 1].control_link^ := new_stack[0];
 			end;
-
-writeln('here 4');
-
 		new_stack[stack_size - 1].access_link^ := new_stack[0];
         new_stack[stack_size - 1].return_address := stack_size - 1;
-
- writeln('here 5');
 
         {* Set the temp values *}
         new_stack[stack_size - 2].ret.var_name := new_stack[stack_size - 1].ret.var_name;
         new_stack[stack_size - 2].ret.var_type := new_stack[stack_size - 1].ret.var_type;
 
- writeln('here 6');
-       
-        	{* Return the new stack *}
-            callFunction := new_stack;
+
+        {* Return the new stack *}
+        callFunction := new_stack;
 
 	end;
 
@@ -169,6 +157,6 @@ function functionReturn( func_array: array of function_info; stack: activation_r
         readln(return_val);
         return_stack[stack_size - 2].ret.var_value := return_stack[stack_size - 1].ret.var_value;
         functionReturn := return_stack;
-        	end;
+    end;
 
 end.
